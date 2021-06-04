c26af2394e759310e4cc2ee03b6af397b27ce829
RichiCorrea <richi.correa.f@gmail.com>
2021-06-04 15:57:49 +0000

    todo OK

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ let cardsArray = new Array;
 let drawCardsButton = document.getElementById("draw-button");
 let sortCardsButton = document.getElementById("sort-button");
 let suitsCard = ["♠","♦","♥","♣"];
-let numsCard = ["2","3","4","5","6","7","8","9","10","J","Q","K","A"];
+let numsCard = ["A","2","3","4","5","6","7","8","9","10","J","Q","K"];
 
 //Class card
 class Card {
